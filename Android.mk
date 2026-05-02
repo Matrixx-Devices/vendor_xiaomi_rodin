cc0dbbf94f477455b994ed8e4eefc7709500c1fa
Rubens Gonçalves <93195322+RubensGon@users.noreply.github.com>
2026-05-02 06:02:00 -0300

    Import OS 3.0.10.0 blobs

---
+++ Android.mk
@@ -0,0 +1,32 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),rodin)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,32bcfe55984b9aed71cb8cebe3fd5624bde5c613)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,e27371155234fee505fabfd7c13d9b65559bf19b)
+$(call add-radio-file-sha1-checked,radio/ccu.img,4349832a14a3449540203f21a08579f08cd00a5e)
+$(call add-radio-file-sha1-checked,radio/connsys_bt.img,8c1b14fa6cd24628aa9007863706bf8d867ebf26)
+$(call add-radio-file-sha1-checked,radio/connsys_gnss.img,2c42e71e0af208dd27f786dc206ec027056f18a0)
+$(call add-radio-file-sha1-checked,radio/connsys_wifi.img,66cac36ded38275c9d0daf8ad0dd9857215021d9)
+$(call add-radio-file-sha1-checked,radio/countrycode.img,8579212cbe9906e68108ff252782b6cb346de1d6)
+$(call add-radio-file-sha1-checked,radio/dpm.img,2faa3cb60460ff2319a171db4f07a708cf4936b3)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,bdb27e1e63b676104c10954f0b2621a4c16a9a8f)
+$(call add-radio-file-sha1-checked,radio/gz.img,182c1b9d09ff1d822e384ff98fd5e1bcb822de87)
+$(call add-radio-file-sha1-checked,radio/lk.img,1d752084ddbd27e0eadd602e91a905473b35b3f9)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,dce548ebddd118149e817972b7a53eed0b1a0a47)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,f08ffc855d364d98a590bf7b3ac61394ad8d801e)
+$(call add-radio-file-sha1-checked,radio/modem.img,115e3589961529d6e5a8fb3d9b00524caf4679de)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,89f18b8f5925ea216fbce8f4d0f9232fd612f9b4)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,8c0e90507e18bc4304022521d863399ae32fa088)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,9eabb9b69659841723c53de3a1d2626707772e5a)
+$(call add-radio-file-sha1-checked,radio/scp.img,b85743228223a03f928b92a158f36741a628ed07)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,5b2c979e2512daa70d395c2755413edb96fdf162)
+$(call add-radio-file-sha1-checked,radio/sspm.img,5f4f232f2cba7e30f1cb0c03fddea3a1db5a0dd9)
+$(call add-radio-file-sha1-checked,radio/tee.img,ce3ffef2ef4070c34733be161a6eeea44e85e1ca)
+$(call add-radio-file-sha1-checked,radio/vcp.img,33f810f23dcb790cdb3e4ba8591c6c65470d897d)
+
+endif
